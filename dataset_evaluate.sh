7180b07d26f68b0390543abaf2c74c04d2cdfcda
shenzhun <shenzhunallen@gmail.com>
2013-11-07 21:40:21 +0800

    add all words of docs in datasets in one file

--- dataset_evaluate.sh
+++ dataset_evaluate.sh
@@ -2,7 +2,8 @@
 # Date: 2013-11-7
 # Author: shen zhun
 
+find ./data/enron -name '*.txt' -exec cat {} \; > all_words
 echo caculate word frequency
-tr -sc '[A-Z][a-z]' '[\012*]' < test | sort | uniq -c | sort -n > test.fq
+tr -sc '[A-Z][a-z]' '[\012*]' < all_words | sort | uniq -c | sort -n > test.fq
 echo check the first 80 word frequency
 cat test.fq | tail -80
